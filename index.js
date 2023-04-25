901d255850c0ffcbd9a4680173d6f8f2998cc3f6
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-04-26 00:25:32 +0530

    feat: Added function so that QR is generated only if URL is valid

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const express = require ('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-var QRCode = require('qrcode');
-const urlValidator = require('url-validator');
+const QRCode = require('qrcode');
+const validUrl = require('valid-url');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/create',async (req,res)=>{
-    try {
-        const url = req.body.url;
-       
-            const Qrcode = await QRCode.toBuffer(url);
-            res.set('Content-Type', 'image/png');
-            res.send(Qrcode);
-       
-            // return res.status(404).json({
-            //     Message:"Invalid URL"
-            // })
-        }
-     catch (error) {
-        return res.status(500).json({
-            message:"Something went wrong"
-        })
+app.post('/create', async (req, res) => {
+  try {
+    const url = req.body.url;
+
+    if (!validUrl.isUri(url)) {
+      return res.status(400).json({
+        message: 'Invalid URL'
+      });
     }
-});  
 
+    const qrCode = await QRCode.toBuffer(url);
+    res.set('Content-Type', 'image/png');
+    res.send(qrCode);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Something went wrong'
+    });
+  }
+});
 
-app.listen(3000,async ()=>{
-      console.log('Server started on 3000');
-})
+app.listen(3000, () => {
+  console.log('Server started on 3000');
+});
