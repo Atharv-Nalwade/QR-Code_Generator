cfad97d19ab2f970e902e7e2435fef7dcc94485d
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-04-26 00:39:37 +0530

    style:Added eslint for better code formatting and code cleanliness

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const QRCode = require('qrcode');
-const validUrl = require('valid-url');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const QRCode = require('qrcode')
+const validUrl = require('valid-url')
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/create', async (req, res) => {
   try {
-    const url = req.body.url;
+    const url = req.body.url
 
     if (!validUrl.isUri(url)) {
       return res.status(400).json({
         message: 'Invalid URL'
-      });
+      })
     }
 
-    const qrCode = await QRCode.toBuffer(url);
-    res.set('Content-Type', 'image/png');
-    res.send(qrCode);
+    const qrCode = await QRCode.toBuffer(url)
+    res.set('Content-Type', 'image/png')
+    res.send(qrCode)
   } catch (error) {
     return res.status(500).json({
       message: 'Something went wrong'
-    });
+    })
   }
-});
+})
 
 app.listen(3000, () => {
-  console.log('Server started on 3000');
-});
+  console.log('Server started on 3000')
+})
